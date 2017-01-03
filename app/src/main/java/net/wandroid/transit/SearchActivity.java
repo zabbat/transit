a45faebfecb036517c42d618136ecd0cdf9a0e33
Johan Jungbeck <johan.jungbeck@wandroid.net>
2017-01-03 11:31:40 +0000

    Add result activity
    Once result is downloaded it should
    be displayed in a spearate activity

--- app/src/main/java/net/wandroid/transit/SearchActivity.java
+++ app/src/main/java/net/wandroid/transit/SearchActivity.java
@@ -1,7 +1,11 @@
 package net.wandroid.transit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        Button searchButton = (Button) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SearchActivity.this,ResultActivity.class));
+            }
+        });
     }
 }
