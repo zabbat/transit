ed62dad13c3fce3c82055e05ded8821b8669cc58
Johan Jungbeck <johan.jungbeck@wandroid.net>
2017-01-04 23:40:00 +0000

    Make SearchActivity zoom on berlin
    The test data is of Berlin so
    Berlin's lat long will be used.
    This should be changed to myLocation

--- app/src/main/java/net/wandroid/transit/ui/SearchActivity.java
+++ app/src/main/java/net/wandroid/transit/ui/SearchActivity.java
@@ -7,6 +7,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
 import net.wandroid.transit.R;
 import net.wandroid.transit.model.Transit;
 import net.wandroid.transit.retrofit.api.ServiceGenerator;
@@ -18,17 +24,26 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class SearchActivity extends AppCompatActivity implements Callback<Transit> {
+public class SearchActivity extends AppCompatActivity implements Callback<Transit>, OnMapReadyCallback {
 
 
-    public static final String TAG = SearchActivity.class.getCanonicalName();
-    public static final String LOCAL_JSON_FILE = "data.json";
-    public static final String HTTP_LOCAL_FILE = "http://local.file";
+    private static final String TAG = SearchActivity.class.getCanonicalName();
+    private static final String LOCAL_JSON_FILE = "data.json";
+    private static final String HTTP_LOCAL_FILE = "http://local.file";
+    private static final float DEFAULT_ZOOM = 12f;
+    /**
+     * Lat lng of Berlin.
+     */
+    private static final LatLng DEFAULT_LAT_LNG = new LatLng(52.528187, 13.410404);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         Button searchButton = (Button) findViewById(R.id.search_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,5 +70,12 @@ public class SearchActivity extends AppCompatActivity implements Callback<Transi
     @Override
     public void onFailure(Call<Transit> call, Throwable t) {
         Log.d(TAG, "failed to get json data: " + t.getMessage());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //TODO: Use myLocation as start point
+        LatLng currentLocation = DEFAULT_LAT_LNG;
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, DEFAULT_ZOOM));
     }
 }
